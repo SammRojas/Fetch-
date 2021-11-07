3d9e961d9b4e6734aa952b524fb87e920943ec59
samuelrojas <samm-rojas@hotmail.com>
2021-11-06 21:03:54 -0500

    Agrego la imagen de avatar de cada usuario

--- 36-fetch.js
+++ 36-fetch.js
@@ -1,22 +1,42 @@
 "use strict";
 /* Fech (ajax) y peticiones aservicios / api rest */
 
-var div_usuarios = document.querySelector("#usuarios");
+var div_usuarios = document.querySelector("#usuarios"); //selecciona el contenedor donde se musetra la informacion
 
-fetch("https://reqres.in/api/users?page=1")
+getUsers()
   .then((data) => data.json())
-  .then((users) => {  
+  .then((users) => {
     listUsers(users.data);
   });
 
+function getUsers() {
+  return fetch("https://reqres.in/api/users?page=1"); // peticion al servicio reqres.in
+}
+
+function getAvatar(user) {
+  /* 
+  Obtiene el avatar de los usuarios 
+  @param {json}
+  @return string
+  */
+  console.log(user.avatar);
+  return user.avatar;
+}
+
 function listUsers(users) {
+  /* 
+  Lista el response obtenido por medio de fetch y lo printa en el DOM 
+  @param  {json}
+  */
   users.map((user, i) => {
     let name = document.createElement("h3");
+    let avatar = document.createElement("IMG");
     name.innerHTML = `${i + 1}.  ${user.first_name}  ${
       user.last_name
     }<span style="color: green;"> correo electronico: </span>${user.email}`;
-
+    avatar.src = getAvatar(user);
     div_usuarios.appendChild(name);
+    div_usuarios.appendChild(avatar);
     document.querySelector(".loading").style.display = "none";
   });
 }
